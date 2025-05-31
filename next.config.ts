998e935c86305e88173293c85956e38fb68c4939
Alvin Pratama <113691381+palveeen22@users.noreply.github.com>
2025-06-01 01:51:37 +0300

    update: config project

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,29 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ['cdn.dummyjson.com', 'i.dummyjson.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.dummyjson.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.dummyjson.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  experimental: {
+    optimizePackageImports: ['@/components', '@/lib'],
+  },
+  compiler: {
+    removeConsole: process.env.NODE_ENV === 'production',
+  },
 };
 
 export default nextConfig;
